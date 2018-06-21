261f4ac985111e50dbae17ceb5b0d78f4ad3e250
Patrick Schiel <p.schiel@gmail.com>
2018-06-21 23:15:39 +0200

    cleanup

--- libs/bootstrap.php
+++ libs/bootstrap.php
@@ -1,26 +1,24 @@
 <?php
 
 // include all necessary classes
-require_once APPDIR . '/config/core.php';
-require_once APPDIR . '/config/database.php';
-require_once COREDIR . '/libs/app.php';
-require_once COREDIR . '/libs/request.php';
-require_once COREDIR . '/libs/controller.php';
-require_once COREDIR . '/libs/component.php';
-require_once COREDIR . '/libs/model.php';
-require_once COREDIR . '/libs/view.php';
-require_once COREDIR . '/libs/helper.php';
-require_once COREDIR . '/libs/db.php';
-require_once COREDIR . '/libs/session.php';
-require_once COREDIR . '/libs/cache.php';
-require_once APPDIR . '/controllers/app_controller.php';
-require_once APPDIR . '/models/app_model.php';
+require APPDIR . '/config/core.php';
+require APPDIR . '/config/database.php';
+require COREDIR . '/libs/app.php';
+require COREDIR . '/libs/request.php';
+require COREDIR . '/libs/controller.php';
+require COREDIR . '/libs/component.php';
+require COREDIR . '/libs/model.php';
+require COREDIR . '/libs/view.php';
+require COREDIR . '/libs/helper.php';
+require COREDIR . '/libs/db.php';
+require COREDIR . '/libs/session.php';
+require COREDIR . '/libs/cache.php';
+require APPDIR . '/controllers/app_controller.php';
+require APPDIR . '/models/app_model.php';
 foreach (glob(APPDIR . '/interfaces/*.php') as $filename) {
 	include $filename;
 }
-if (@file_exists(APPDIR . '/vendor/autoload.php')) {
-    include APPDIR . '/vendor/autoload.php';
-}
+require APPDIR . '/vendor/autoload.php';
 
 // error handling
 ini_set('display_errors', DEBUG ? 1 : 0);
@@ -29,7 +27,7 @@ ini_set('log_errors', 1);
 ini_set('error_log', ERRORLOG);
 ini_set('log_errors_max_len', 150000);
 
-// utf8, disable browser cache
+// set utf8
 if (ini_get('default_charset')) {
 	ini_set('default_charset', 'UTF-8');
 } else {
